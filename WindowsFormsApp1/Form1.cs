77ec8fb4114a1fa5452ad20de2b257c13fd3d807
pasratel1 <artajove@list.ru>
2019-09-19 21:31:24 +0400

    4

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,32 +13,32 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         Bitmap bmp;
-        List<MouseEventArgs> Coordinates;
+        List<Point> Coordinates;
         public Form1()
         {
             InitializeComponent();
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            Coordinates = new List<MouseEventArgs>();
+            Coordinates = new List<Point>(); 
         }
 
-        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) //событие (функция вызывается, когда нажата кнопка мыши)
         {
-            
-            if (e.Button == MouseButtons.Left) //если лкм
+            var p = new Point(e.X, e.Y); //вызов конструктора создание объекта
+            if (e.Button == MouseButtons.Left) //если нажата левая кнопка мыши, то выполняется (делается черный пиксель)
             {
-                bmp.SetPixel(e.X, e.Y, Color.Black); //делаем пиксель черным
-                Coordinates.Add(e);//добавляем в листок с точками
+                bmp.SetPixel(p.X, p.Y, Color.Black); //делаем пиксель черным
+                Coordinates.Add(p);//добавляем в листок с точками
             }
-            else if (e.Button == MouseButtons.Right)//если пкм
+            else if (e.Button == MouseButtons.Right)//если правая
             {
                 Random random = new Random();//создаем объект рандома
-                var pointColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)); //выбираем рандомный цвет
-                foreach (MouseEventArgs Coordinate in Coordinates) //проходим по листку 
+                var pointColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)); 
+                foreach (Point Coordinate in Coordinates) //проходим по координатам
                 {
-                    bmp.SetPixel(Coordinate.X, Coordinate.Y, pointColor); //меняем цвет точек на рандомный
+                    bmp.SetPixel(Coordinate.X, Coordinate.Y, pointColor); 
                 }
             }
-            pictureBox1.Image = bmp; //отображаем
+            pictureBox1.Image = bmp; 
         }
     }
 }
